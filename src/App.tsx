9a39bc30964927994e4f7a06796de60d5d2a6461
yuki-kai <74969849+yuki-kai@users.noreply.github.com>
2022-05-14 17:07:47 +0900

    ルーティング設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { Button, ChakraProvider } from "@chakra-ui/react";
-import "./styles.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+
+import theme from "./theme/theme";
+import { Router } from "./router/Router";
 
 export default function App() {
   return (
-    <ChakraProvider>
-      <Button>ボタン</Button>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
