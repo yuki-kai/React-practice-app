80932bac1a164d4d8d4bd399f78b623fc203dabf
yuki-kai <74969849+yuki-kai@users.noreply.github.com>
2022-05-15 17:05:05 +0900

    レイアウト作成

---
+++ src/components/organisms/layout/Header.tsx
@@ -0,0 +1,40 @@
+import { Box, Flex, Heading, Link, useDisclosure } from "@chakra-ui/react";
+import { memo, VFC } from "react";
+
+import { MenuIconButton } from "../../atoms/button/MenuIconButoon";
+import { MenuDrawer } from "../../molebules/MenuDrawer";
+
+export const Header: VFC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Flex
+        as="nav"
+        bg="teal.500"
+        color="gray.50"
+        align="center"
+        justify="space-between"
+        padding={{ base: 3, md: 5 }}
+      >
+        <Flex align="center" as="a" mr={8} _hover={{ cousor: "pointer" }}>
+          <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+            ユーザー管理アプリ
+          </Heading>
+        </Flex>
+        <Flex
+          align="center"
+          fontSize="sm"
+          flexGrow={2}
+          display={{ base: "none", md: "flex" }}
+        >
+          <Box pr={4}>
+            <Link>ユーザー一覧</Link>
+          </Box>
+          <Link>設定</Link>
+        </Flex>
+        <MenuIconButton onOpen={onOpen} />
+      </Flex>
+      <MenuDrawer onClose={onClose} isOpen={isOpen} />
+    </>
+  );
+});
